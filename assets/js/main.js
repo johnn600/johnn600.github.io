9d1adadb02fa8bb43c9b903ea79e50ebb479db78
johnn600 <johnrey.vilbar@msugensan.edu.ph>
2023-09-19 00:32:59 +0800

    added js animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,39 @@ jQuery(document).ready(function($) {
    
 
 });
+
+
+//reference: https://codepen.io/dbilanoski/pen/LabpzG?editors=1111
+const sections = document.querySelectorAll("section[id]");
+console.log(sections);
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 150;
+    sectionId = current.getAttribute("id");
+
+    console.log(sectionId);
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector("section#"+sectionId).classList.add("section-active");
+    } else {
+        document.querySelector("section#"+sectionId).classList.remove("section-active");
+    }
+  });
+}
+
